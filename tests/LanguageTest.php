e5165fb0bcd065746a3093063aa0fe9a57525447
jane-olszewska <jane.olszewska@duedil.com>
2018-10-10 11:26:03 +0100

    Added ability to auto-polyfill ::function in a given namespace

--- tests/LanguageTest.php
+++ tests/LanguageTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace IrRegular\Tests\Hopper;
 
+use function IrRegular\Hopper\add_function_constant_polyfill_to_ns;
 use function IrRegular\Hopper\get_defined_functions_in_ns;
 use PHPUnit\Framework\TestCase;
 
@@ -24,5 +25,15 @@ class LanguageTest extends TestCase
         // namespace arg tolerant of upper/lowercase, and optional backslash at the end
         $fns = get_defined_functions_in_ns('irregular\tests\\', true);
         $this->assertContains($testSubjectFn, $fns);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testAutoPolyfillingFunctionsInNamespace()
+    {
+        add_function_constant_polyfill_to_ns('IrRegular\Tests\Hopper');
+
+        new \ReflectionClass('IrRegular\Tests\Hopper\testsubject');
     }
 }
