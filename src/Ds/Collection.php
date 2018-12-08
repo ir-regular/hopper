66eee6d7f1d4a8a9d80d70495774717dd032fbbe
jane-olszewska <jane.olszewska@duedil.com>
2018-12-08 13:48:23 +0000

    Collection implements IteratorAggregate interface as a fallback
    Some data structures aren't sequential, but we need this fallback to fit them into existing language tools.
    All hopper functions expect an iterable, as that is the most general interface available for PHP aggregates.

--- src/Ds/Collection.php
+++ src/Ds/Collection.php
@@ -5,8 +5,13 @@ namespace IrRegular\Hopper\Ds;
 
 /**
  * Represents a collection that holds elements but does not specify the access method.
+ *
+ * Extends \IteratorAggregate only because I want the collections to fit the `iterable` slot.
+ *
+ * All hopper functions prefer interacting with appropriate interface methods from
+ * `IrRegular\Hopper\Ds` interfaces, if possible.
  */
-interface Collection extends \Countable
+interface Collection extends \Countable, \IteratorAggregate
 {
     public function isEmpty(): bool;
 
--- src/Ds/Sequence.php
+++ src/Ds/Sequence.php
@@ -3,12 +3,7 @@ declare(strict_types=1);
 
 namespace IrRegular\Hopper\Ds;
 
-/**
- * Extends \IteratorAggregate because I want the collections to fit the `iterable` slot,
- * but that's currently underdeveloped because all the hopper functions prefer interacting
- * with appropriate interface methods from `IrRegular\Hopper` interfaces.
- */
-interface Sequence extends Collection, \IteratorAggregate
+interface Sequence extends Collection
 {
     public function first();
 
