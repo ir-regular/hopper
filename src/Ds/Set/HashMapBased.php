8aff087181190e281a05e47f3c679c4e445c3fcc
jane-olszewska <jane.olszewska@duedil.com>
2018-12-11 21:26:09 +0000

    Simplify HashMapBased set by reusing an EagerHashMap to dedupe values

--- src/Ds/Set/HashMapBased.php
+++ src/Ds/Set/HashMapBased.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 
 namespace IrRegular\Hopper\Ds\Set;
 
+use function IrRegular\Hopper\hash_map;
+use function IrRegular\Hopper\size;
+use IrRegular\Hopper\Ds\HashMap;
 use IrRegular\Hopper\Ds\Lazy;
-use function IrRegular\Hopper\Language\convert_to_key;
-use function IrRegular\Hopper\Language\is_valid_key;
 use IrRegular\Hopper\Ds\Mappable;
 use IrRegular\Hopper\Ds\Set as SetInterface;
 use IrRegular\Hopper\Ds\Vector;
@@ -14,84 +15,64 @@ use IrRegular\Hopper\Ds\Vector\Eager;
 class HashMapBased implements SetInterface
 {
     /**
-     * @var array
+     * @var HashMap
      */
-    protected $uniqueIndex = [];
+    protected $hashMap;
 
-    /**
-     * @var array
-     */
-    protected $array = [];
-
-    public function __construct(array $elements, array $uniqueIndex)
+    public function __construct(iterable $collection)
     {
-        $this->array = $elements;
-        $this->uniqueIndex = $uniqueIndex;
+        $count = size($collection);
+        $values = array_fill(0, $count, true);
+
+        $this->hashMap = hash_map($values, $collection);
     }
 
     // Collection
 
     public function isEmpty(): bool
     {
-        return empty($this->array);
+        return $this->hashMap->isEmpty();
     }
 
     public function count(): int
     {
-        return count($this->array);
+        return $this->hashMap->count();
     }
 
     public function contains($value): bool
     {
-        if (is_object($value) || is_scalar($value)) {
-            // objects or scalars are easy to convert to a string
-
-            if (!is_valid_key($value)) {
-                $value = convert_to_key($value);
-            }
-
-            assert(is_string($value) || is_int($value));
-
-            return array_key_exists($value, $this->uniqueIndex);
-
-        } else {
-            // we don't really want to convert anything more complicated into a string if we don't have to
-            // so just search for the value
-
-            return (array_search($value, $this->array, true) !== false);
-        }
+        return $this->hashMap->isKey($value);
     }
 
     public function getValues(): iterable
     {
-        return $this->array;
+        return $this->hashMap->getKeys();
     }
 
     public function getIterator()
     {
-        return new \ArrayIterator($this->array);
+        return new \ArrayIterator($this->hashMap->getKeys());
     }
 
     // Foldable
 
     public function foldl(callable $closure, $initialValue)
     {
-        return array_reduce($this->array, $closure, $initialValue);
+        return array_reduce($this->hashMap->getKeys(), $closure, $initialValue);
     }
 
     public function foldr(callable $closure, $initialValue)
     {
-        return array_reduce(array_reverse($this->array), $closure, $initialValue);
+        return array_reduce(array_reverse($this->hashMap->getKeys()), $closure, $initialValue);
     }
 
     // Mappable
 
     public function map(callable $closure): Mappable
     {
-        $newValues = array_map($closure, $this->array);
-        $newIndex = array_map('\IrRegular\Hopper\Language\convert_to_key', $newValues);
+        $newValues = array_map($closure, $this->hashMap->getKeys());
 
-        return new self($newValues, $newIndex);
+        return new self($newValues);
     }
 
     public function lMap(callable $closure): Lazy
@@ -109,6 +90,6 @@ class HashMapBased implements SetInterface
 
     public function toVector(): Vector
     {
-        return new Eager($this->array);
+        return new Eager($this->hashMap->getKeys());
     }
 }
--- src/set.php
+++ src/set.php
@@ -3,28 +3,10 @@ declare(strict_types=1);
 
 namespace IrRegular\Hopper;
 
-use function IrRegular\Hopper\Language\convert_to_key;
-use function IrRegular\Hopper\Language\is_valid_key;
 use IrRegular\Hopper\Ds\Set;
 use IrRegular\Hopper\Ds\Set\HashMapBased;
 
 function set(iterable $collection): Set
 {
-    $elements    = [];
-    $uniqueIndex = [];
-
-    foreach ($collection as $element) {
-        $key = is_valid_key($element)
-            ? $element
-            : convert_to_key($element);
-
-        $elementAdded = !array_key_exists($key, $uniqueIndex);
-
-        if ($elementAdded) {
-            $elements[] = $element;
-            $uniqueIndex[$key] = count($elements) - 1;
-        }
-    }
-
-    return new HashMapBased($elements, $uniqueIndex);
+    return new HashMapBased($collection);
 }
